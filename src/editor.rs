93f7c69da7a4bc9f94c2d002dcb8aa0e9018cb87
W Santos <51366379+ahzzano@users.noreply.github.com>
2025-11-09 17:33:11 +0800

    Add hello world window

---
+++ src/editor.rs
@@ -0,0 +1,25 @@
+use iced::{widget::text, Element};
+
+#[derive(Default)]
+pub struct Editor;
+
+#[derive(Debug, Clone)]
+pub enum Message {}
+
+impl Editor {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn title(&self) -> String {
+        String::from("moon")
+    }
+
+    pub fn update(&mut self, message: Message) {
+        match message {}
+    }
+
+    pub fn view(&self) -> Element<'_, Message> {
+        text("Hello world!").into()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+pub mod editor;
+
+use editor::Editor;
+
+#[tokio::main]
+async fn main() {
+    let _ = iced::application(Editor::title, Editor::update, Editor::view).run();
 }
